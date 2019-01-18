6e70c3a0e30ca806df22f72921de13073df76891
himanshupathak21061998 <hpathak336@gmail.com>
2019-01-19 00:19:16 +0530

    The seq2seq model is completed

--- model.py
+++ model.py
@@ -8,8 +8,6 @@ Created on Tue Jan 15 01:08:45 2019
 
 import tensoflow as tf
 import numpy as np
-import re
-import time
 
 #Building the seq2seq model
 class Model():
@@ -81,4 +79,25 @@ class Model():
             training_predictions = self.decode_training_set(encoder_state,decoder_cell,sequence_length,decoding_scope,output_function,keep_prob,batch_size)
             test_predictions = self.decode_test_set(encoder_state,decoder_cell,decoder_embedding_matrix,word2int['<SOS>'],word2int['<EOS>'],sequence_length-1,num_words,decoding_scope,output_function,keep_prob,batch_size)
         return training_predictions,test_predictions
+    
+    # Building the seq2seq model
+    def seq2seq_model(self, inputs, targets, keep_prob, batch_size, sequence_length, answers_num_words, questions_num_words, encoder_embedding_size, decoder_embedding_size, rnn_size, num_layers, questionswords2int):
+        encoder_embedded_input = tf.contrib.layers.embed_sequence(inputs,
+                                                              answers_num_words + 1,
+                                                              encoder_embedding_size,
+                                                              initializer = tf.random_uniform_initializer(0, 1))
+        encoder_state = self.encoder_rnn(encoder_embedded_input, rnn_size, num_layers, keep_prob, sequence_length)
+        preprocessed_targets = self.preprocess_targets(targets, questionswords2int, batch_size)
+        decoder_embeddings_matrix = tf.Variable(tf.random_uniform([questions_num_words + 1, decoder_embedding_size], 0, 1))
+        decoder_embedded_input = tf.nn.embedding_lookup(decoder_embeddings_matrix, preprocessed_targets)
+        training_predictions, test_predictions = self.decoder_rnn(decoder_embedded_input,
+                                                         decoder_embeddings_matrix,
+                                                         encoder_state,
+                                                         questions_num_words,
+                                                         sequence_length,
+                                                         rnn_size,
+                                                         num_layers,
+                                                         questionswords2int,
+                                                         keep_prob,
+                                                         batch_size)
     
